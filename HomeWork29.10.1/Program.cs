1ea91c8ac697ea0ffca5df54c7a3635c71ababd7
Svarog13 <102320657+Svarog13@users.noreply.github.com>
2023-10-30 21:33:17 +0200

    Update Program.cs

--- HomeWork29.10.1/Program.cs
+++ HomeWork29.10.1/Program.cs
@@ -1,100 +1,108 @@
-﻿Money money = new Money(9,28);
-Product product = new Product("Kartoplya",new Money(10,50));
-
-//money.Print();
-
-System.Console.WriteLine(money.ToString());
-//System.Console.WriteLine(product);
-product.Print();
-class Money
+class Program
 {
-	private int dollar;
-	private int cent;
-
-	public int Dollar
-	{
-		get { return dollar; }
-		set { dollar = value; }
-	}
-	public int Cent
-	{
-		get { return cent; }
-		set { cent = value; }
-	}
-
-	public Money() : base() { }
-
-	public Money(int d, int c)
-	{
-		dollar = d;
-		cent = c;
-	}
-	public virtual void Print()
-	{
-		System.Console.WriteLine($"Dollar: {dollar}");
-		System.Console.WriteLine($"Cent: {cent}");
-	}
-	public override string ToString()
-	{
-		return $"Price : {dollar}.{cent}";
-	}
-	public void AddSum(int dollar, int cent)
-	{
-		if (cent > 99)
-		{
-			dollar++;
-			cent = this.cent - 100;
-			//this.dollar += cent / 100;
-		}
-		else
-		{
-            this.dollar += dollar;
-            this.cent += cent;
-        }
-	}
-}
-
-class Product : Money
-{
-	private string name;
-	private Money price;
-
-	public string Name
-	{
-		get { return name; }
-		set { name = value; }
-	}
-	public Money Price
-	{
-		get { return price; }
-		set { price = value; }
-	}
-	public Product() : base() { name = "No name"; }
-    public Product(string n, Money p)
-	{
-		this.name = n;
-		this.price = p;
-	}
-	public void Substraction(int dollar, int cent)
-	{
-		price.Dollar -= dollar;
-		price.Cent -= cent;
-		if (cent > price.Cent)
-		{
-			price.Dollar -= 1;
-			price.Cent = price.Cent + 100 - cent;
-        }
-		if (price.Dollar < 0 || price.Cent < 0)
-		{
-			price.Dollar = 0;
-			price.Cent = 0;
-		}
-	}
-    public override void Print()
+    static void Main(string[] args)
     {
-		System.Console.WriteLine($"Product name - {name}");
-        Console.WriteLine($"{price.Dollar}.{price.Cent}");
-		//base.Print();
-	}
+        Money money = new Money(9, 28);
+        Product product = new Product("Potato", new Money(10, 50));
 
+        System.Console.WriteLine("Money object:");
+        money.Print();
+        Console.WriteLine(money);
+
+        Console.WriteLine("\nProduct object:");
+        product.Print();
+        System.Console.WriteLine(product);
+
+        Console.WriteLine("\nSybstracting 2 dollars and 30 cents from the prodect price: ");
+        product.Substraction(2, 30);
+        product.Print();
+
+    }
+    class Money
+    {
+        private int dollar;
+        private int cent;
+
+        public int Dollar
+        {
+            get { return dollar; }
+            set { dollar = value; }
+        }
+        public int Cent
+        {
+            get { return cent; }
+            set { cent = value; }
+        }
+
+        public Money() : base() { }
+
+        public Money(int d, int c)
+        {
+            dollar = d;
+            cent = c;
+        }
+        public virtual void Print()
+        {
+            System.Console.WriteLine($"Dollar: {dollar}");
+            System.Console.WriteLine($"Cent: {cent}");
+        }
+        public override string ToString()
+        {
+            return $"Price : {dollar}.{cent}";
+        }
+        public void AddSum(int dollars, int cent)
+        {
+            dollar += dollars;
+            cent += cent;
+
+            if (cent > 99)
+            {
+                dollar += cent / 100;
+                cent %= 100;
+            }
+        }
+    }
+
+    class Product : Money
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Product() : base() { name = "No name"; }
+        public Product(string n, Money p)
+        {
+            name = n;
+            Dollar = p.Dollar;
+            Cent = p.Cent;
+        }
+        public void Substraction(int dollars, int cent)
+        {
+            Dollar -= dollars;
+            Cent -= cent;
+
+            if (Cent < 0)
+            {
+                Dollar -= 1;
+                Cent = 100 + Cent;
+            }
+
+            if (Dollar < 0)
+            {
+                Dollar = 0;
+                Cent = 0;
+            }
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Product name: {name}");
+            Console.WriteLine(this);
+
+        }
+    }
 }
+
